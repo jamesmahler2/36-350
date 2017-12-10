fecda325bb19ee82d9441aaf20db78e551204213
jamesmahler2 <jmahler@andrew.cmu.edu>
2017-12-09 22:50:35 -0500

    ayyy

---
+++ alien_message.R
@@ -0,0 +1,3 @@
+cat("Hi! I am James, from planet Earth. The majority of our planet is water, and most of the rest is green 
+    grass/trees. What sources of energy(food, water) do you guys use to survive? What are the temperatures
+    there like? Would you be interested in having humans from Earth go to your planet to do some research?")
